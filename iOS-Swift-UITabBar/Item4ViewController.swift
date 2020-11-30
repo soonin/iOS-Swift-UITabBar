03008625a5d9a12a3f5078f43b2a2f37e26224a5
Soonin Challenge <34075996+soonin@users.noreply.github.com>
2020-11-30 02:07:11 -0800

    UITabBar
    IOS , Swift , Tutorial - create a UITabBar Multi View  (UITabBar)

---
+++ iOS-Swift-UITabBar/Item4ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Item4ViewController.swift
+//  iOS-Swift-UITabBar
+//
+//  Created by Pooya on 2020-11-29.
+//  Copyright © 2020 centurytrail.com. All rights reserved.
+//
+
+import UIKit
+
+class Item4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
